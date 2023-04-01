10a2737e17e83a03625f959934a05bcbbae8eccb
Ruben Alvarez <dev.ralvarez@gmail.com>
2023-04-01 18:37:47 +0200

    Algo de responsive

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="text-center">
       <header className="flex flex-col items-center justify-center min-h-screen">
-        <div className="bg-gray-800 w-1/3 rounded-2xl">
-          <div className="text-white text-center text-5xl m-3">
+        <div className="bg-gray-800 md:w-1/3 rounded-2xl">
+          <div className="text-white text-center text-3xl lg:text-5xl m-3">
               Calculator
           </div>
 
@@ -19,78 +19,78 @@ function App() {
             </div>
           </div>
 
-          <div className="flex items-stretch h-24">
+          <div className="flex items-stretch h-16 lg:h-24">
             <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
-              <button className="h-20 w-20 rounded-3xl bg-red-400 hover:bg-red-500" alt="Delete last number or operation">x</button>
+              <button className="h-14 w-14 lg:h-20 lg:w-20 rounded-3xl bg-red-400 hover:bg-red-500" alt="Delete last number or operation">x</button>
             </div>
             <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
-              <button className="h-20 w-20 rounded-3xl bg-gray-500 hover:bg-gray-600" alt="Open parentheses">(</button>
+              <button className="h-14 w-14 lg:h-20 lg:w-20 rounded-3xl bg-gray-500 hover:bg-gray-600" alt="Open parentheses">(</button>
             </div>
             <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
-              <button className="h-20 w-20 rounded-3xl bg-gray-500 hover:bg-gray-600" alt="Close parentheses">)</button>
+              <button className="h-14 w-14 lg:h-20 lg:w-20 rounded-3xl bg-gray-500 hover:bg-gray-600" alt="Close parentheses">)</button>
             </div>
             <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
-              <button className="h-20 w-20 rounded-3xl bg-gray-500 hover:bg-gray-600" alt="Split operation">/</button>
-            </div>
-          </div>
-          
-          <div className="flex items-stretch h-24">
-            <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
-              <button className="h-20 w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add number 7">7</button>
-            </div>
-            <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
-              <button className="h-20 w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add number 8">8</button>
-            </div>
-            <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
-              <button className="h-20 w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add number 9">9</button>
-            </div>
-            <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
-              <button className="h-20 w-20 rounded-3xl bg-gray-500 hover:bg-gray-600" alt="Multiplication operation">*</button>
-            </div>
-          </div>
-
-          <div className="flex items-stretch h-24">
-            <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
-              <button className="h-20 w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add number 4">4</button>
-            </div>
-            <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
-              <button className="h-20 w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add number 5">5</button>
-            </div>
-            <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
-              <button className="h-20 w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add number 6">6</button>
-            </div>
-            <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
-              <button className="h-20 w-20 rounded-3xl bg-gray-500 hover:bg-gray-600" alt="Subtract operation">-</button>
+              <button className="h-14 w-14 lg:h-20 lg:w-20 rounded-3xl bg-gray-500 hover:bg-gray-600" alt="Split operation">/</button>
             </div>
           </div>
           
-          <div className="flex items-stretch h-24">
+          <div className="flex items-stretch h-16 lg:h-24">
             <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
-              <button className="h-20 w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add number 1">1</button>
+              <button className="h-14 w-14 lg:h-20 lg:w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add number 7">7</button>
             </div>
             <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
-              <button className="h-20 w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add number 2">2</button>
+              <button className="h-14 w-14 lg:h-20 lg:w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add number 8">8</button>
             </div>
             <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
-              <button className="h-20 w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add number 3">3</button>
+              <button className="h-14 w-14 lg:h-20 lg:w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add number 9">9</button>
             </div>
             <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
-              <button className="h-20 w-20 rounded-3xl bg-gray-500 hover:bg-gray-600" alt="Addition operation">+</button>
+              <button className="h-14 w-14 lg:h-20 lg:w-20 rounded-3xl bg-gray-500 hover:bg-gray-600" alt="Multiplication operation">*</button>
             </div>
           </div>
 
-          <div className="flex items-stretch h-24">
+          <div className="flex items-stretch h-16 lg:h-24">
             <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
-              <button className="h-20 w-20 rounded-3xl bg-red-400 hover:bg-red-500" alt="Delete all operation">C</button>
+              <button className="h-14 w-14 lg:h-20 lg:w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add number 4">4</button>
             </div>
             <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
-              <button className="h-20 w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add number 0">0</button>
+              <button className="h-14 w-14 lg:h-20 lg:w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add number 5">5</button>
             </div>
             <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
-              <button className="h-20 w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add comma">,</button>
+              <button className="h-14 w-14 lg:h-20 lg:w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add number 6">6</button>
             </div>
             <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
-              <button className="h-20 w-20 rounded-3xl bg-blue-400 hover:bg-blue-500" alt="Calculate mathematical operation">=</button>
+              <button className="h-14 w-14 lg:h-20 lg:w-20 rounded-3xl bg-gray-500 hover:bg-gray-600" alt="Subtract operation">-</button>
+            </div>
+          </div>
+          
+          <div className="flex items-stretch h-16 lg:h-24">
+            <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
+              <button className="h-14 w-14 lg:h-20 lg:w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add number 1">1</button>
+            </div>
+            <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
+              <button className="h-14 w-14 lg:h-20 lg:w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add number 2">2</button>
+            </div>
+            <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
+              <button className="h-14 w-14 lg:h-20 lg:w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add number 3">3</button>
+            </div>
+            <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
+              <button className="h-14 w-14 lg:h-20 lg:w-20 rounded-3xl bg-gray-500 hover:bg-gray-600" alt="Addition operation">+</button>
+            </div>
+          </div>
+
+          <div className="flex items-stretch h-16 lg:h-24">
+            <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
+              <button className="h-14 w-14 lg:h-20 lg:w-20 rounded-3xl bg-red-400 hover:bg-red-500" alt="Delete all operation">C</button>
+            </div>
+            <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
+              <button className="h-14 w-14 lg:h-20 lg:w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add number 0">0</button>
+            </div>
+            <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
+              <button className="h-14 w-14 lg:h-20 lg:w-20 rounded-3xl bg-gray-400 hover:bg-gray-500" alt="Add comma">,</button>
+            </div>
+            <div className="flex flex-1 justify-center items-center content-center text-white text-2xl">
+              <button className="h-14 w-14 lg:h-20 lg:w-20 rounded-3xl bg-blue-400 hover:bg-blue-500" alt="Calculate mathematical operation">=</button>
             </div>
           </div>
         </div>
